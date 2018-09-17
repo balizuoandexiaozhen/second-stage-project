1b2924d681614c9144a3f3cc25cbc56a900b513c
balizuoandexiaozhen <balizuoandexiaozhen@github.com>
2018-09-17 18:29:41 +0800

    9.17下午吃饭去了

--- src/views/index/javascripts/modules/nav.js
+++ src/views/index/javascripts/modules/nav.js
@@ -1,8 +1,25 @@
 $.ajax({
     type:"get",
-    url:"/json/header-search.json",
+    url:"/json/nav-category.json",
     datatype:"json",
     success:function(res){
-        
+        var str = '';
+        $.each(res,function(index,val){
+
+            str += `<li>
+                        <span class="nav-li-font">
+                            <i class="iconfont nav-icon">${val.icon}</i>
+                            <a href="">${val.title}</a>    
+                        </span>
+                        <span>></span>
+                    </li>`
+        })
+        $(".nav-list").find("ul").html(str);
+        $(".nav-list").find("li").hover(function() {
+            $(this).css("background", "rgba(2,181,157,.5)")
+            // console.log($(this).css("background"));
+        }, function() {
+            $(this).css("background", "rgba(2,181,157,.7)")
+        });
     }
 });
